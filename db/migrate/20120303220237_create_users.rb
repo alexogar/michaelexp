9fc870075ea272c43e10e0e838ac16e8e2b214c1
alexogar <alexey.ogarkov@gmail.com>
2012-03-29 22:27:34 +0400

    Change limit

--- db/migrate/20120303220237_create_users.rb
+++ db/migrate/20120303220237_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name, :limit =>20
       t.integer :age, :max => 100
-      t.string :sex, :limit => 1
+      t.string :sex, :limit => 20
       t.timestamps
     end
   end
